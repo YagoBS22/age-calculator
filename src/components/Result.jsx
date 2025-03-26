565d64e74c0ac9d631c69418a6ba6a9ba122f438
YagoBS22 <yb.santos2002@gmail.com>
2025-03-26 11:01:15 -0300

    fix Result font size and number format

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -6,21 +6,21 @@ const Result = ({day, month, year}) => {
         <Stack gap={2}>
             <Typography className="result-fonts" variant="h1">
                 <span style={{color:"hsl(259, 100%, 65%)" }}>
-                    {day === "" ? "--" : day}
+                    {day === "" ? "-- " : day+" "}
                 </span>
                 Dias
             </Typography>
 
             <Typography className="result-fonts" variant="h1">
                 <span style={{color:"hsl(259, 100%, 65%)" }}>
-                    {month === "" ? "--" : month}
+                    {month === "" ? "-- " : month+" "}
                 </span>
                 Meses
             </Typography>
 
             <Typography className="result-fonts" variant="h1">
                 <span style={{color:"hsl(259, 100%, 65%)" }}>
-                    {year === "" ? "--" : year}
+                    {year === "" ? "-- " : year+" "}
                 </span>
                 Anos
             </Typography>
